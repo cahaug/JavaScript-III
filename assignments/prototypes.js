8db1c9b72204b8dcf7c4533d28514f66094df48a
cahaug <caleahaug@gmail.com>
2019-04-23 17:09:33 -0700

    partially working prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,3 +1,5 @@
+console.log("=====Prototypes.js=====")
+
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
@@ -16,12 +18,30 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+const GameObject = function (attrs){
+  this.createdAt=attrs.createdAt;
+  this.name=attrs.name;
+  this.dimensions=attrs.dimensions;
+}
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+const CharacterStats = function (attrs){
+  this.healthPoints=attrs.healthPoints
+}
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +52,17 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+const Humanoid = function(attrs){
+  this.team=attrs.team;
+  this.weapons=attrs.weapons;
+  this.language=attrs.language;
+}
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +72,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+// /*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +133,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+// */
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,3 +1,5 @@
+console.log("=====This.js=====")
+
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
